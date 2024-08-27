45187dde01ae6a4bab3fdc6abab9dbc9f44dc511
santosh <santoshugale1991@gmail.com>
2024-08-27 20:49:51 +0530

    Added binary search tree and one problem related to bst

---
+++ Leetcode/Tree/FindPairWithGivenSumInBalancedBinarySearchTree.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using static Leetcode.BinarySearchTree;
+
+namespace Leetcode.Tree
+{
+    public class FindPairWithGivenSumInBalancedBinarySearchTree
+    {
+        /// <summary>
+        /// Given a BST and a sum, find if there is a pair with given sum.
+        /// </summary>
+        /// <param name="bt"></param>
+        /// <param name="givenSum"></param>
+        /// <returns></returns>
+        public static bool IsPairExist(BinarySearchTree bt, int givenSum)
+        {
+            var sortedList = new List<int>();
+            ToListInSortedOrder(bt.Root, sortedList);
+            var startIndex = 0;
+            var endIndex = sortedList.Count - 1;
+            while (startIndex < endIndex)
+            {
+                var addition = sortedList[startIndex] + sortedList[endIndex];
+                if (addition == givenSum)
+                    return true;
+                else if (addition < givenSum)
+                    endIndex--;
+                else
+                    startIndex++;
+            }
+
+            return false;
+        }
+
+        // sorting is basically in-order traversal
+        public static void ToListInSortedOrder(BSTNode node, List<int> list)
+        {
+            if (node == null) return;
+            ToListInSortedOrder(node.Left, list); // recur to left
+            list.Add(node.Data);
+            ToListInSortedOrder(node.Right, list); // recur to right
+
+        }
+    }
+}
---
+++ LeetcodeUnitTest/Tree/BinarySearchTreeTests.cs
@@ -0,0 +1,102 @@
+using Leetcode;
+using Xunit;
+
+namespace LeetcodeUnitTest.Tree
+{
+    public class BinarySearchTreeTests
+    {
+        [Fact]
+        public void Insert_WhenCalled_InsertsDataIntoBST()
+        {
+            // Arrange
+            var bst = new BinarySearchTree();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(7);
+
+            // Act
+            var root = bst.Root;
+
+            // Assert
+            Assert.Equal(5, root.Data);
+            Assert.Equal(3, root.Left.Data);
+            Assert.Equal(7, root.Right.Data);
+        }
+
+        [Fact]
+        public void InsertUsingLoop_WhenCalled_InsertsDataIntoBST()
+        {
+            // Arrange
+            var bst = new BinarySearchTree();
+            bst.InsertUsingLoop(5);
+            bst.InsertUsingLoop(3);
+            bst.InsertUsingLoop(7);
+
+            // Act
+            var root = bst.Root;
+
+            // Assert
+            Assert.Equal(5, root.Data);
+            Assert.Equal(3, root.Left.Data);
+            Assert.Equal(7, root.Right.Data);
+        }
+
+        [Fact]
+        public void PrintPostOrder_WhenCalled_PrintsPostOrderTraversal()
+        {
+            // Arrange
+            var bst = new BinarySearchTree();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(7);
+
+            // Act
+            var output = CaptureConsoleOutput(() => BinarySearchTree.PrintPostOrder(bst.Root));
+
+            // Assert
+            Assert.Equal("3 7 5 ", output);
+        }
+
+        [Fact]
+        public void PrintPreOrder_WhenCalled_PrintsPreOrderTraversal()
+        {
+            // Arrange
+            var bst = new BinarySearchTree();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(7);
+
+            // Act
+            var output = CaptureConsoleOutput(() => BinarySearchTree.PrintPreOrder(bst.Root));
+
+            // Assert
+            Assert.Equal("5 3 7 ", output);
+        }
+
+        [Fact]
+        public void PrintInOrder_WhenCalled_PrintsInOrderTraversal()
+        {
+            // Arrange
+            var bst = new BinarySearchTree();
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(7);
+
+            // Act
+            var output = CaptureConsoleOutput(() => BinarySearchTree.PrintInOrder(bst.Root));
+
+            // Assert
+            Assert.Equal("3 5 7 ", output);
+        }
+
+        private string CaptureConsoleOutput(Action action)
+        {
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            action.Invoke();
+
+            return consoleOutput.ToString();
+        }
+    }
+}
