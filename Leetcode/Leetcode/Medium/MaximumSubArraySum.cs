0122d5570b3266772082b7e0e56f30d056c73e6d
santosh <santoshugale1991@gmail.com>
2024-08-27 20:32:24 +0530

    Added leetcode medium problem solutions

---
+++ Leetcode/Leetcode/Medium/MaximumSubArraySum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Leetcode.Medium
+{
+    // Given an array arr[] of size N.
+    // The task is to find the sum of the contiguous subarray within a arr[] with the largest sum.
+    // Kadane’s Algorithm
+    public class MaximumSubArraySum
+    {
+        public static (int, int, int) MaxiMumSubArraySumBruteForce(int[] array)
+        {
+            int max_so_far = int.MinValue, max_ending_here = 0, start = 0, end = 0, tempStart = 0;
+            for (int index = 0; index < array.Length; index++)
+            {
+                max_ending_here = max_ending_here + array[index];
+                if (max_ending_here > max_so_far)
+                {
+                    max_so_far = max_ending_here;
+                    start = tempStart;
+                    end = index;
+                }
+                if (max_ending_here < 0)
+                {
+                    max_ending_here = 0;
+                    tempStart = index + 1;
+                }
+            }
+            return (max_so_far, start, end);
+        }
+    }
+}
